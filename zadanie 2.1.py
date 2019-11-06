521a92e44472dc41a48e898ce0c8f419f39e2f5c
Maciej Wąsiatycz <56920706+macwascdv@users.noreply.github.com>
2019-11-06 01:11:18 +0100

    Update zadanie 2.1.py

--- zadanie 2.1.py
+++ zadanie 2.1.py
@@ -1,21 +1,4 @@
-print("Program do oceniania")
-y=int(input("wpisz uzyskana ilosc punktow ucznia: "))
-maxp=40
-x=(y/maxp)*100
-x=float(x)
-if (x<39):
-    print("Uczen dostaje ocene ndst")
-elif(x>40 and x<49):
-     print("uzyskany procent: {}".format(x))
-     print("uczen dostaje ocene dop")
-elif(x>50 and x<69):
-      print("uzyskany procent: {}".format(x))
-      print("uczen dostaje ocene dst")
-elif(x>70 and x<84):
-        print("uzyskany procent: {}".format(x))
-        print("uczen dostaje ocene db")
-elif(x>85 and x<99):
-          print("uzyskany procent: {}%".format(x))
-          print("dostaje ocene bdb")
-else:
-    print("Uczen dostal ocene celujaca" )
+print("Program ten przeliteruje wpisane zdanie od tyłu")
+zd=input("Podaj zdanie: ")
+for i in range (0,len(zd)):
+   print(zd[len(zd)-1-i])
